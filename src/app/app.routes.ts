6bbb919d89de346d44ccb0d44a58d0fe3b6a4814
dylansi-pro <dylansi.hi@gmail.com>
2026-06-24 16:15:43 +0200

    Exercice 2 Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Exo2 } from './exercice2/exo2/exo2';
 import { Exo3 } from './exercice3/exo3/exo3';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/exo1', pathMatch: 'full' },
+  { path: '', redirectTo: '/exo2', pathMatch: 'full' },
   { path: 'exo1', component: Exo1 },
   { path: 'exo2', component: Exo2 },
   { path: 'exo3', component: Exo3 },
--- src/app/exercice2/exo2/exo2.ts
+++ src/app/exercice2/exo2/exo2.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+export interface Operation {
+  id: number;
+  heure: string;
+  expression: string;
+  resultat: number;
+}
 
 @Component({
   selector: 'app-exo2',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './exo2.html',
   styleUrl: './exo2.css',
 })
-export class Exo2 {}
+export class Exo2 {
+  n1 = 0;
+  n2 = 0;
+  operation = '+';
+  resultat: number | null = null;
+  historique: Operation[] = [];
+
+  calculer() {
+    let res = 0;
+    switch(this.operation) {
+      case '+': res = this.n1 + this.n2; break;
+      case '-': res = this.n1 - this.n2; break;
+      case '*': res = this.n1 * this.n2; break;
+      case '/': res = this.n1 / this.n2; break;
+    }
+    this.resultat = res;
+    
+    // Ajout à l'historique
+    this.historique.unshift({
+      id: Date.now(),
+      heure: new Date().toLocaleTimeString(),
+      expression: `${this.n1} ${this.operation} ${this.n2}`,
+      resultat: res
+    });
+  }
+
+  supprimer(id: number) {
+    this.historique = this.historique.filter(op => op.id !== id);
+  }
+}
